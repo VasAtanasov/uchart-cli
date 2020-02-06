43aecacaef20b3e88e5ee377436d0a9b9b38fb8e
VasAtanasov <vas.atanasov@gmail.com>
2020-02-06 18:23:27 +0000

    SAVEPOINT

--- uchart/parsers.py
+++ uchart/parsers.py
@@ -82,7 +82,7 @@ class JrcParser:
     def run(self, args):
         if args.cmd != 'jrc':
             return False
-        logger.debug("Running Generate:run")
+        logger.debug("Running JrcParser:run")
 
         ctx = get_context()
         ctx.file = args.file
@@ -94,6 +94,70 @@ class JrcParser:
         macro.add(OpenCsvFile())
         macro.add(ParseJAN9201Content())
         macro.add(GenerateXmlFIle())
+
+        macro.run(ctx)
+
+        return True
+
+
+@uchart_plugin
+class Converter:
+
+    @classmethod
+    def get_argparser(cls, parser):
+        """
+            Returns a base parser with common arguments for the generate plugin
+        """
+        init_parser = parser.add_parser(
+            "cnv",
+            help="Generates Furuno ECDIS usermap from JRC JAN 9201 ECIDS usermap csv file."
+        )
+
+        init_parser.add_argument(
+            "file",
+            metavar="<file>",
+            help="file with user chart elements",
+            type=str
+        )
+
+        init_parser.add_argument(
+            "type",
+            metavar="<type>",
+            help="destination file typ",
+            type=str
+        )
+
+    def run(self, args):
+        if args.cmd != 'cnv':
+            return False
+        logger.debug("Running JrcParser:run")
+
+        ctx = get_context()
+        ctx.file = args.file
+        ctx.type = args.type
+
+        import json
+
+        with open(ctx.file) as f:
+            data = json.load(f)
+
+        import csv
+
+        with open('route.csv', mode='w', newline='') as csv_file:
+            fieldnames = ['name', 'latitude', 'longitude']
+            writer = csv.DictWriter(
+                csv_file, fieldnames=fieldnames, extrasaction='ignore')
+
+            writer.writeheader()
+            for waypoint in data["routes"][0]["waypoints"]:
+                writer.writerow(waypoint)
+                lat = waypoint["latitude"]
+                lon = waypoint["longitude"]
+                name = waypoint["name"]
+
+        macro = Macro()
+
+        macro.add(IsFile())
 
         macro.run(ctx)
 
